c363162081a8b1c7441405288ed7a335de7a6aba
Rodrigo Morais <morais.rm@gmail.com>
2015-08-17 13:02:00 -0300

    Draw circle in canvas.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,86 @@
 'use strict';
+
+var canvas;
+
+var configCanvas = function(){
+  var height = window.innerHeight * 0.9,
+      top = window.innerHeight * 0.15,
+      width = window.innerWidth;
+
+  canvas = document.getElementById('draw-area')
+
+  $(canvas)
+    .css('backgroundColor', '#000000')
+    .height(height)
+    .width(width);
+
+  paper.setup(canvas);
+};
+
+var createElement = function(){
+  var type = $('#type').val(),
+      left = $('#left').val(),
+      top = $('#top').val(),
+      color = $('#color').val(),
+      edge = $('#edge').val(),
+      radius = $('#radius').val(),
+      width = $('#width').val(),
+      height = $('#height').val();
+
+  if(type === 'Circle'){
+    drawCircle(parseInt(radius), parseInt(left), parseInt(top), color, edge);
+  }
+  else if(type === 'Rectangle'){
+    createRectangle(left, top, color, edge, width, height, true);
+  }
+};
+
+var drawCircle = function(radius, left, top, backcolor, edge){
+  var circle;
+  
+  circle = new Path.Circle({
+    radius: radius,
+    fillColor: backcolor,
+    strokeColor: edge,
+    x: left,
+    y: top
+  });
+};
+
+
+$('#color, #edge')
+  .css('backgroundColor', '#FF0000');
+
+$('button').on('click', function(e){
+  e.preventDefault();
+
+  createElement();
+});
+
+$('#color, #edge').change(function() {
+    $(this)
+      .css(
+          'backgroundColor',
+          $(this)
+            .find('option:selected')
+            .css('backgroundColor')
+      );
+});
+
+$('#type').change(function() {
+  if($(this).val() === 'Circle'){
+    $('.circle').css('display', 'inline-block');
+    $('.rectangle').css('display', 'none');
+  }
+  else
+  {
+    $('.circle').css('display', 'none');
+    $('.rectangle').css('display', 'inline-block');
+  }
+});
+
+$('.circle').css('display', 'inline-block');
+$('.rectangle').css('display', 'none');
+
+paper.install(window);
+configCanvas();
